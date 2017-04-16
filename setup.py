2b088c15d98a8593a75dad92c0acd89b25463d33
song.jie <song.jie@cesgroup.com.cn>
2017-04-17 00:18:07 +0800

    add basic

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SalesTruly',
+    version='1.0.0',
+    packages=['Bundle'],
+    url='',
+    license='MIT',
+    author='songjie',
+    author_email='songjie1984@gmail.com',
+    description=''
+)
